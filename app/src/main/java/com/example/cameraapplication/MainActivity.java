e6c05073bdf53c870532fd79d92b38f213a9e6d9
EzgiAydinhan <ezgi.aydinhan@boutiquerugs.com>
2023-08-29 21:19:21 +0300

    Request.java eklendi.

--- app/src/main/java/com/example/cameraapplication/MainActivity.java
+++ app/src/main/java/com/example/cameraapplication/MainActivity.java
@@ -7,15 +7,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Base64;
-import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -51,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     Button cameraBtn,galleryBtn;
     Button uploadBtn;
     String currentPhotoPath;
+    Camera mCamera;
+    SurfaceView mPreview;
 
     private ProgressDialog processDialog;
     //StorageReference storageReference;
